fb8a980e26843cbb98696f40cbe350d1af5de421
Peter Kwok <peter.kwok94@gmail.com>
2025-02-18 23:02:14 -0500

    fix: render failed deploy, re-built updated dist

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
-export const apiURL = "http://localhost:10000"; // testing
-// export const apiURL =
-//   "https://amazon-like-ecommerce-project-express.onrender.com"; // production
+// export const apiURL = "http://localhost:10000"; // testing
+export const apiURL =
+  "https://amazon-like-ecommerce-project-express.onrender.com"; // production
